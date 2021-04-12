4974926e7e7b5ff3562d5d188fc58305e044d766
jiye9701@gmail.com <jiye9701@gmail.com>
2021-04-11 21:56:50 -0400

    Add project files.

---
+++ assignment6/assignment6/_vti_cnf/assignment6.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Aug 2020 00:27:14 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|zho-PC\\zho
+vti_modifiedby:SR|zho-PC\\zho
+vti_timecreated:TR|18 Aug 2020 05:30:21 -0000
+vti_backlinkinfo:VX|Desktop/jiye/assignment6/assignment6/assignment6/assignment6/assignment6.html
+vti_cacheddtm:TX|19 Aug 2020 00:27:14 -0000
+vti_filesize:IR|713
---
+++ assignment6/assignment6/assignment6.js
@@ -0,0 +1,31 @@
+﻿var imgIndex = 1;
+var timer = null;
+
+showImg(imgIndex);
+
+function changeImage(n) {
+    clearTimeout(timer);
+    showImg(imgIndex += n);
+}
+
+function currentSlide(n) {
+    clearTimeout(timer);
+    showImg(imgIndex = n);
+}
+
+function showImg(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n == undefined) { n = ++imgIndex }
+    if (n > slides.length) { imgIndex = 1 }
+    if (n < 1) { imgIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[imgIndex - 1].style.display = "block";
+
+    //Image changes automatically in 5 seconds
+    timer = setTimeout(showImg, 5000);
+} 
+
